61398a060b997748f45384c8fba1a9c41004b313
Marcos <marcos.canal779@comunidadunir.net>
2024-03-07 21:35:33 +0100

    Pequeño cambio

--- src/app/components/usuario-card/usuario-card.component.ts
+++ src/app/components/usuario-card/usuario-card.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, inject } from '@angular/core';
 import { IUsuario } from '../../interfaces/usuario.interfaces';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { UsuariosService } from '../../services/usuarios.service';
 
 @Component({
@@ -15,11 +15,13 @@ export class UsuarioCardComponent {
   @Input() miUsuario!: IUsuario;
 
   usuarioService = inject(UsuariosService)
+  router = inject(Router)
 
   async borrarUser() {
     let response = confirm('Seguro que quiere borrar el usuario con id: ' + this.miUsuario._id);
      if(response){
       let res = await this.usuarioService.delete(this.miUsuario._id);
+      this.router.navigate(['/users'])
       if(res){
         alert('Se ha borrado correctamente')
       }
